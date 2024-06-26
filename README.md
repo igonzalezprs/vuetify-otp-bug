f59a6a806b95248fd7c70d98689b27947c854447
Isidro Gonzalez <igonzalezprs@gmail.com>
2024-06-26 11:38:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# vuetify-otp-bug
+
+Repo created to showcase Vuetify's v-otp-input bug with autofill.
+
+- Vuetify Version: 3.6.10
+- Vue version: 3.4.30
---
+++ src/components/OtpTest.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-centerfill-height mx-auto" max-width="900">
+      <v-otp-input v-model="input" />
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const input = ref('');
+</script>
